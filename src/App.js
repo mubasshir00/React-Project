70658c47909c1dea374808bdd1b99079c85f2aee
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2021-03-17 06:58:18 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data'
+import { useState } from 'react';
+import List from './List';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  btn:{
+    margin: theme.spacing(1) ,
+    
+  }
+}));
+
 
 function App() {
+  const classes = useStyles();
+
+  const [People, setPeople] = useState(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="list">
+        
+        <Grid container spacing={1}>
+          <Grid container item xs={12} spacing={3}>
+            <List className={classes.paper} People={People} />
+        </Grid>
+        </Grid>    
+
+       <div className={classes.btn}>
+          <Button className={classes.btn} variant="contained" color="primary" onClick={()=>setPeople(data)}>
+            Refresh 
+         </Button>
+          <Button className={classes.btn} variant="contained" color="secondary" onClick={() => setPeople([])}>
+            Clear All
+          </Button>
+       </div>
+        
+       </div>
     </div>
   );
 }
